a9fd0858486e279d4d58b90110e2f2ea4cf12a9d
Aurora Evalon <ellaevalon122342@gmail.com>
2023-12-01 11:42:28 -0500

    Day 1 Fully Complete. Part 2 is jank but it works :3

--- Day 1/TrebuchetPart2.py
+++ Day 1/TrebuchetPart2.py
@@ -2,10 +2,27 @@ import regex
 import math
 
 finalNumber = 0
-data = open("Trebuchet_input.txt", "r")
+data = open("Day 1\Trebuchet_input.txt", "r")
 fileData = data.read()
 splitData = fileData.split()
 for x in splitData:
+    x = x.replace("twone", "21")
+    x = x.replace("nineight", "98")
+    x = x.replace("threeight", "38")
+    x = x.replace("oneight", "18")
+    x = x.replace("fiveight", "58")
+    x = x.replace("sevenine", "79")
+    x = x.replace("eighthree", "83")
+    x = x.replace("eightwo", "82")
+    x = x.replace("one", "1")
+    x = x.replace("two", "2")
+    x = x.replace("three", "3")
+    x = x.replace("four", "4")
+    x = x.replace("five", "5")
+    x = x.replace("six", "6")
+    x = x.replace("seven", "7")
+    x = x.replace("eight", "8")
+    x = x.replace("nine", "9")
     numberData = regex.sub("[a-z]", "", x)
     firstNumber = numberData[:1]
     lastNumber = numberData[-1]
