43ad50a11e9a4c1f524a0ffe3444aa5cfbbd9e54
Aurora Evalon <ellaevalon122342@gmail.com>
2023-12-04 08:18:21 -0500

    I'm stupid ignore this one its just to backup

--- Day 2/cube.py
+++ Day 2/cube.py
@@ -1,11 +1,25 @@
+import regex
+
 text = open("Day 2\\cubeInput.txt", "r")
 data = text.read()
 
+handsList=[]
 game = 0
 splitData = data.split("\n")
 for x in splitData:
     game += 1
     formattedData = x[5:]
-    set = formattedData.split(";")
-    print(game)
-    print(set)
+    formattedData = formattedData.split(":")
+    set = formattedData[1].split(";")
+    for x in set:
+        hands = x.split(",")
+        handsList += hands
+    handColor = regex.split("a-m", hands)
+    handAmount = regex.split("1-9", hands)
+    for x in handsList:
+        if handColor == "blue" and handAmount <= 14:
+            blueValid = "true"
+        else:
+            blueValid = "false"
+    
+    handsList=[]
